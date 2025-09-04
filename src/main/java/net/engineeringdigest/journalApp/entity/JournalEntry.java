7cb89dc057373bf2f90ee51d7b5b163aa92f88e2
Shubhamkale1 <kaleshubham602@gmail.com>
2025-09-04 15:29:32 +0530

    initial cooments

---
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -0,0 +1,34 @@
+package net.engineeringdigest.journalApp.entity;
+
+public class JournalEntry {
+
+    private long id;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    private String content;
+}
