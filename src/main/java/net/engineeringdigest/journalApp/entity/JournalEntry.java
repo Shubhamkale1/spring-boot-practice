c4cdf482b4b94813a87c82a01f0aa61eef1526c8
Shubhamkale1 <kaleshubham602@gmail.com>
2025-09-13 20:45:55 +0530

    added spring security

--- src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -1,34 +1,24 @@
 package net.engineeringdigest.journalApp.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "journal_entries")
+@Data
+@NoArgsConstructor
 public class JournalEntry {
-
-    private long id;
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    private String name;
+    @Id
+    private ObjectId id;
+    @NonNull
+    private String title;
 
     private String content;
+
+    private LocalDateTime date;
 }
